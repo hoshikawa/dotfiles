1ba4e0cffe9a46659d67d347532bc074b71bcd73
hoshikawa <1301601+hoshikawa@users.noreply.github.com>
2026-04-28 14:34:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-  #!/bin/bash
-  mkdir -p "$HOME/.config/git"
-  ln -sf "$HOME/dotfiles/.config/git/ignore" "$HOME/.config/git/ignore"
+#!/bin/bash
+mkdir -p "$HOME/.config/git"
+ln -sf "$HOME/dotfiles/.config/git/ignore" "$HOME/.config/git/ignore"
+
+mkdir -p "$HOME/.gemini"
+ln -sf "$HOME/dotfiles/.gemini/settings.json" "$HOME/.gemini/settings.json"
